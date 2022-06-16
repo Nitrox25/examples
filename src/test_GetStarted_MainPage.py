1bce4d95c45d54bdcff5975aca68cff732a87d33
Leonid Khanin <han094@yandex.ru>
2022-06-16 20:24:44 +0300

    fix test_GetStarted_MainPage.py

--- src/test_GetStarted_MainPage.py
+++ src/test_GetStarted_MainPage.py
@@ -73,8 +73,52 @@ class Test_GetStarted_MainPage:
         # TODO подкладывать файл из шага 5
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
             (By.XPATH, "//div[@id=\'app\']/main/div/main/div/div/section/div[2]/div/div/a"))).click()
-        self.driver.find_element(By.ID, "fileUpload").send_keys("./storage/wallets/9DDercNf9n.ctr")
+        self.driver.find_element(By.ID, "fileUpload").send_keys("/Users/leokhanin/Downloads/2e26tH2xqA.ctr")
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
             (By.CSS_SELECTOR, ".inputWrapper__input"))).send_keys("1")
         self.driver.find_element(By.LINK_TEXT, "Auth").click()
 
+def test_MainPage(self):
+    WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".select-market__selectIcon > use"))).click()
+    WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".table__tbody-row:nth-child(1) > .marketTable__market"))).click()
+
+# self.driver.find_element(By.CSS_SELECTOR, ".select-market__selectIcon").click()
+# self.driver.find_element(By.CSS_SELECTOR, ".table__tbody-row:nth-child(2) > .marketTable__market").click()
+# self.driver.find_element(By.CSS_SELECTOR, ".select-market__selectIcon").click()
+# self.driver.find_element(By.CSS_SELECTOR, ".table__tbody-row:nth-child(3) > .marketTable__market").click()
+
+
+@pytest.mark.usefixtures("setup")
+class Test_restore:
+    def test_GetStarted_button(self):
+        element = self.driver.find_element(By.XPATH, "//section/div/div/div/a")
+        assert element.text == "GET STARTED"
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.XPATH, "//section/div/div/div/a"))).click()
+
+    def test_Restore_button(self):
+        element = self.driver.find_element(By.XPATH, "//div[@id=\'app\']/main/div/main/div/div/div/div[3]/button")
+        assert element.text == "Restore"
+        self.driver.find_element(By.XPATH, "//div[@id=\'app\']/main/div/main/div/div/div/div[3]/button").click()
+
+    def test_Restoring_container(self):
+        element = self.driver.find_element(By.XPATH, "//*[@id=\'app\']/div[2]/div[2]/div/div/div[2]/label[1]/span[1]")
+        element1 = self.driver.find_element(By.XPATH, "//*[@id=\'app\']/div[2]/div[2]/div/div/div[2]/label[2]/span[1]")
+        assert element.text == "enter your private key here"
+        assert element1.text == "enter new passcode"
+        time.sleep(1)
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, ".focus > .inputWrapper__input"))).send_keys(
+            "9W61Zb7vB6PxDLRxYDGAZ9JTE8gBPm9it9HuXfFp5ojkyxapYyFPX9zd3PJj3jj6KYmBhiNoyRr4TbgnosjapwYRmxWZB")
+
+        self.driver.find_element(By.XPATH,
+                                 "//div[@id=\'app\']/div[2]/div[2]/div/div/div[2]/label[2]/span[2]/input").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".focus > .inputWrapper__input").send_keys("1")
+
+
+    def test_Restoring_container_saving(self):
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(
+            (By.LINK_TEXT, "Restore"))).click()
+    # TODO  Checking вышестоящего контейнера - имя ?
