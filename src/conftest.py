4130bf27d7840ecd296a84355ac8d97df45387b1
Leonid Khanin <han094@yandex.ru>
2022-06-16 17:54:15 +0300

    fix conftest.py

--- src/conftest.py
+++ src/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
 from os import environ
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 @pytest.fixture(scope="class")
 def setup(request):
     print("initiating chrome driver")
+
     SELNAME = environ.get('SELNAME')
     if SELNAME is None:
         SELNAME = "localhost"
@@ -16,15 +16,14 @@ def setup(request):
     HOSTNAME = environ.get('HOSTNAME')
     if HOSTNAME is None:
         HOSTNAME = "localhost"
+    print(SELNAME, SELPORT, HOSTNAME)
 
-    driver = webdriver.Remote(
+    chrome_driver = webdriver.Remote(
         command_executor=f'http://{SELNAME}:{SELPORT}/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME,
-        # options=webdriver.ChromeOptions()
+
+        options=webdriver.ChromeOptions()
     )
-    driver.get(f"https://demo.megadex.com/")
-    driver.maximize_window()
-    request.cls.driver = driver
-    yield driver
-    driver.close()
-    print("\n I'm the fixture - tearDown")
+    chrome_driver.get(f"{HOSTNAME}")
+    request.cls.driver = chrome_driver
+    yield
+    chrome_driver.quit()
